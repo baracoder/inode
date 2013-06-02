f17ddaacbfed1f947f4152b14e121c6fc88a9ed8
Herman Fries <baracoder@googlemail.com>
2013-06-02 02:48:31 +0200

    erlaubt redmine auth zu umgehen zum entwickeln

--- config.example.js
+++ config.example.js
@@ -1,7 +1,8 @@
 var config = {
     port : 3000,
     storagePath : './storage',
-    minTags: 3
+    minTags: 3,
+    bypassAuth: false // usefull for local development
 };
 
 module.exports = config;
